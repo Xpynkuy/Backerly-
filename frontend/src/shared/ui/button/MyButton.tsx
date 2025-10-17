49db388096150517a01c7e9f9ac84f6d8bc2e6df
Andrew <xpynkuy3@gmail.com>
2025-10-17 07:14:03 +0500

    create btm component

---
+++ frontend/src/shared/ui/button/MyButton.tsx
@@ -0,0 +1,63 @@
+import type { ReactNode } from "react";
+import styles from "./MyButton.module.scss";
+
+const ButtonSize = {
+  SMALL: "small",
+  MEDIUM: "medium",
+  LARGE: "large",
+  FULL: "full",
+} as const;
+
+const ButtonColor = {
+  PRIMARY: "primary",
+  ACCENT: "accent",
+  GREEN: "green",
+  GRAY: "gray",
+  RED: "red",
+} as const;
+
+interface ButtonProps {
+  children?: ReactNode;
+  size?: keyof typeof ButtonSize;
+  color?: keyof typeof ButtonColor;
+  className?: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  type?: "button" | "submit" | "reset";
+}
+
+const MyButton = (props: ButtonProps) => {
+  const {
+    children,
+    size = "MEDIUM",
+    color = "PRIMARY",
+    className = "",
+    onClick,
+    disabled = false,
+    type = "button",
+  } = props;
+
+  const sizeValue = ButtonSize[size];
+  const colorValue = ButtonColor[color];
+
+  const buttonClasses = `
+    ${styles.btn} 
+    ${styles[`btn--${sizeValue}`]} 
+    ${styles[`btn--${colorValue}`]} 
+    ${className}
+  `
+    .trim()
+    .replace(/\s+/g, " ");
+  return (
+    <button
+      className={buttonClasses}
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default MyButton;
