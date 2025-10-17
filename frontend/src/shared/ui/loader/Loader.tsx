f33a02ef0e462dbeb153645c8d85bd13b93a1af7
Andrew <xpynkuy3@gmail.com>
2025-10-17 07:13:52 +0500

    add custom loader

---
+++ frontend/src/shared/ui/loader/Loader.tsx
@@ -0,0 +1,12 @@
+import styles from "./Loader.module.scss";
+
+const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <span className={styles.loader__text}>loading</span>
+      <span className={styles.load}></span>
+    </div>
+  );
+};
+
+export default Loader;
