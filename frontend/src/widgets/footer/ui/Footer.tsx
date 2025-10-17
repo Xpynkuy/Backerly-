7f963dab254d00489fe6fed63a4e3e8299025ee2
Andrew <xpynkuy3@gmail.com>
2025-10-17 07:13:39 +0500

    create footer

---
+++ frontend/src/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.scss";
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span>© 2025 All rights reserved </span>
+    </footer>
+  );
+};
