a254bd8a2ef39abc46173df2fca1ca03181a9c90
Andrew <xpynkuy3@gmail.com>
2025-10-17 07:11:49 +0500

    add alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, "src/app"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@widgets": path.resolve(__dirname, "src/widgets"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+});
