1697a99a12c8ae0e47be2a7832e1f00d7137b7da
Xpynkuy <andrey_pgg@mail.ru>
2025-11-09 19:48:55 +0500

    add db config

---
+++ backend/src/config/db.ts
@@ -0,0 +1,13 @@
+import prisma from "./prisma";
+
+export async function connentDB() {
+  try {
+    console.log("Попытка подключения к БД");
+    await prisma.$connect();
+    console.log("✅ Подключено к БД");
+    return true;
+  } catch (error) {
+    console.log("❌ Ошибка подключения к БД", error);
+    throw error;
+  }
+}
