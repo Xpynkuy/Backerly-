6be5eb0d4195fa6f323fdc0dcd11c2ed7444d0f1
Xpynkuy <andrey_pgg@mail.ru>
2025-12-02 22:10:07 +0500

    update auth controller

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from "express";
-import { loginUser, registerUser } from "../service/authService";
+import {
+  loginUser,
+  logout,
+  refreshTokens,
+  registerUser,
+} from "../service/authService";
+
 
 class AuthController {
   async registration(req: Request, res: Response) {
     try {
       const { username, password } = req.body;
+
+      if (!username || !password) {
+        return res
+          .status(400)
+          .json({ error: "Username and password are required" });
+      }
       const user = await registerUser(username, password);
       res.status(201).json({ userId: user.id, username: user.username });
     } catch (error: any) {
@@ -15,10 +27,67 @@ class AuthController {
   async login(req: Request, res: Response) {
     try {
       const { username, password } = req.body;
-      const { token, user } = await loginUser(username, password);
-      res.json({ token, user });
+
+      if (!username || !password) {
+        return res.status(400).json("Username and password are required");
+      }
+
+      const { accessToken, refreshToken, user } = await loginUser(
+        username,
+        password
+      );
+
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+      res.json({ accessToken, user });
     } catch (error: any) {
       res.status(401).json({ error: error.message });
+    }
+  }
+
+  async refresh(req: Request, res: Response) {
+    try {
+      const { refreshToken } = req.cookies;
+
+      if (!refreshToken) {
+        return res.status(401).json({ error: "Refresh token required" });
+      }
+
+      const tokens = await refreshTokens(refreshToken);
+
+      res.cookie("refreshToken", tokens.refreshToken, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+
+      res.json({
+        accessToken: tokens.accessToken,
+      });
+    } catch (error: any) {
+      res.status(401).json({ error: error.message });
+    }
+  }
+
+  async logout(req: Request, res: Response) {
+    try {
+      const { refreshToken } = req.cookies;
+
+      if (refreshToken) {
+        await logout(refreshToken);
+      }
+
+      res.clearCookie("refreshToken", {
+        httpOnly: true,
+        sameSite: "strict",
+      });
+
+      res.json({ message: "Logged out successfully" });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
     }
   }
 }
