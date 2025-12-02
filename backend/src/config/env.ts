500488d14434ba03f907ab4e698948ee4e2f192f
Xpynkuy <andrey_pgg@mail.ru>
2025-12-02 22:09:50 +0500

    update env.ts config

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,10 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 5001;
-export const jwtSecret = process.env.JWT_SECRET;
+export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
-if (!jwtSecret) {
+export const ACCESS_TOKEN_EXPIRES_IN = '15m';
+export const REFRESH_TOKEN_EXPIRES_IN = '7d';
+
+if (!JWT_ACCESS_SECRET || !JWT_REFRESH_SECRET) {
     throw new Error("JWT_SECRET is not defined in environment variables. Please set it in .env file.");
 }
 
-export const JWT_SECRET = jwtSecret; 
+ 
