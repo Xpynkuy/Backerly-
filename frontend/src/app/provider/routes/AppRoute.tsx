5273eaa0312c5c96530750773ce998f49b289a30
Xpynkuy <andrey_pgg@mail.ru>
2025-12-05 20:48:46 +0500

    update route

--- frontend/src/app/provider/routes/AppRoute.tsx
+++ frontend/src/app/provider/routes/AppRoute.tsx
@@ -1,15 +1,47 @@
-import { MainPage } from "@pages/main";
-import { Layout } from "@widgets/layout";
-import { Route, Routes } from "react-router";
+import {MainPage} from "@pages/main";
+import {Layout} from "@widgets/layout";
+import {Route, Routes} from "react-router";
+import {LoginPage} from "@pages/login";
+import {RegisterPage} from "@pages/register";
+import {AuthLayout} from "@widgets/authLayout";
+import {ProtectedRoute} from "@shared/lib/protectedRoute/ProtectedRoute.tsx";
+import {Profile} from "@pages/profile";
+import {Feed} from "@pages/feed";
 
 const AppRoute = () => {
   return (
+    //MAIN LAYOUT
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<MainPage/>}></Route>
-        <Route></Route>
-        <Route></Route>
+      <Route
+        element={<Layout />}
+      >
+        <Route
+          path="/"
+          element={<MainPage />}
+        ></Route>
+        <Route
+          path='/profile'
+          element={<ProtectedRoute><Profile /></ProtectedRoute>}
+        ></Route>
+        <Route
+          path='/feed'
+          element={<ProtectedRoute><Feed /></ProtectedRoute>}
+        ></Route>
       </Route>
+
+
+      //AUTH LAYOUT
+      <Route element={<AuthLayout />}>
+        <Route
+          path='/login'
+          element={<LoginPage />}
+        ></Route>
+        <Route
+          path='/register'
+          element={<RegisterPage />}
+        ></Route>
+      </Route>
+
     </Routes>
   );
 };
