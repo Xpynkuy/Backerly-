417ac2ca3a8307c73402e23a7e3eb71e868fee65
Xpynkuy <andrey_pgg@mail.ru>
2025-12-05 20:54:57 +0500

    fix loader

--- frontend/src/shared/ui/loader/Loader.tsx
+++ frontend/src/shared/ui/loader/Loader.tsx
@@ -2,9 +2,11 @@ import styles from "./Loader.module.scss";
 
 const Loader = () => {
   return (
-    <div className={styles.loader}>
-      <span className={styles.loader__text}>loading</span>
-      <span className={styles.load}></span>
+    <div className={styles.loaderWrapper}>
+      <div className={styles.loader}>
+        <span className={styles.loader__text}>loading</span>
+        <span className={styles.load}></span>
+      </div>
     </div>
   );
 };
