1082049f744c52265129ce36709565f1d33218dc
Xpynkuy <andrey_pgg@mail.ru>
2025-12-08 23:11:24 +0500

    add not found page

---
+++ frontend/src/pages/notFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,20 @@
+import { useTranslation } from "react-i18next";
+import styles from "./NotFoundPage.module.scss";
+import MyButton from "@shared/ui/button/MyButton";
+import { Link } from "react-router-dom";
+
+export const NotFoundPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.error}>404</h1>
+        <h2 className={styles.title}>{t("Blog not found")}</h2>
+        <span className={styles.desc}>{t("Sorry, the blog not found.")}</span>
+        <Link to='/' className={styles.link}>
+          <MyButton size="AUTO">{t("TO HOME PAGE")}</MyButton>
+        </Link>
+      </div>
+    </div>
+  );
+};
