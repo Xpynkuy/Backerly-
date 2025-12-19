e81d5abc0e3dfaddf3a896c4a15be4361f157a79
Andrew <andrey_pgg@mail.ru>
2025-12-19 17:37:26 +0500

    fix form components

--- frontend/src/features/auth/ui/LoginForm/LoginForm.tsx
+++ frontend/src/features/auth/ui/LoginForm/LoginForm.tsx
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from "react";
-import {useLoginMutation} from "@features/auth/model/api/authApi.ts";
-import {Link, useNavigate} from "react-router-dom";
-import {useAppDispatch, useAppSelector} from "@shared/lib/hooks/hooks.ts";
-import styles from './LoginForm.module.scss'
-import {setError} from "@features/auth/model/slice/authSlice.ts";
+import React, { useEffect, useState } from "react";
+import { useLoginMutation } from "@features/auth/model/api/authApi.ts";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "@shared/lib/hooks/hooks.ts";
+import styles from "./LoginForm.module.scss";
+import { setError } from "@features/auth/model/slice/authSlice.ts";
 import MyInput from "@shared/ui/input/MyInput.tsx";
 import MyButton from "@shared/ui/button/MyButton.tsx";
+import { useTranslation } from "react-i18next";
+import { renderWithLineBreaks } from "@shared/lib/utils/renderWithLineBreaks";
 
 interface LoginForm {
   username: string;
@@ -14,21 +16,22 @@ interface LoginForm {
 
 const LoginForm = () => {
   const [formData, setFormData] = useState<LoginForm>({
-    username: '',
-    password: ''
-  })
-  const [login, {isLoading}] = useLoginMutation()
+    username: "",
+    password: "",
+  });
+  const [login, { isLoading }] = useLoginMutation();
 
-  const dispatch = useAppDispatch()
-  const navigate = useNavigate()
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { t } = useTranslation();
 
-  const {isAuthenticated, error} = useAppSelector(state => state.auth)
+  const { isAuthenticated, error } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
     if (isAuthenticated) {
-      navigate('/profile')
+      navigate("/profile");
     }
-  }, [isAuthenticated])
+  }, [isAuthenticated]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -39,61 +42,59 @@ const LoginForm = () => {
     try {
       await login(formData).unwrap();
     } catch (err) {
-      let message = 'Login failed';
+      let message = "Login failed";
 
-      if (typeof err === 'object' && err !== null && 'data' in err) {
+      if (typeof err === "object" && err !== null && "data" in err) {
         const data = (err as any).data;
-        message = data?.error || data?.message || 'Login failed';
+        message = data?.error || data?.message || "Login failed";
       }
       dispatch(setError(message));
     }
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
-    setFormData(prev => ({
+    const { name, value } = e.target;
+    setFormData((prev) => ({
       ...prev,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h3 className={styles.title}>Sign In</h3>
-        <p className={styles.desc}>Enter your credentials to access your account</p>
+        <h3 className={styles.title}>
+          {renderWithLineBreaks(t("loginForm.title"))}
+        </h3>
+        <p className={styles.desc}>
+          {renderWithLineBreaks(t("loginForm.desc"))}
+        </p>
       </div>
-      <form
-        className={styles.form}
-        onSubmit={handleSubmit}
-      >
+      <form className={styles.form} onSubmit={handleSubmit}>
         <MyInput
-          name='username'
+          name="username"
           placeholder="Username"
           value={formData.username}
           onChange={handleChange}
         ></MyInput>
         <MyInput
-          name='password'
+          name="password"
           placeholder="Password"
           value={formData.password}
           onChange={handleChange}
-          type='password'
+          type="password"
           showPasswordToggle={true}
         ></MyInput>
         {error && <div>{error}</div>}
-        <MyButton
-          type='submit'
-          disabled={isLoading}
-          size='FULL'
-        >Sign In</MyButton>
+        <MyButton type="submit" disabled={isLoading} size="FULL">
+          {renderWithLineBreaks(t("loginForm.btn"))}
+        </MyButton>
       </form>
       <div className={styles.navigate}>
-        Don't have ad account?
-        <Link
-          to='/register'
-          className={styles.link}
-        >Create one</Link>
+        {renderWithLineBreaks(t("loginForm.nav"))}
+        <Link to="/register" className={styles.link}>
+          {renderWithLineBreaks(t("loginForm.navLink"))}
+        </Link>
       </div>
     </div>
   );
--- frontend/src/features/auth/ui/RegisterForm/RegisterForm.tsx
+++ frontend/src/features/auth/ui/RegisterForm/RegisterForm.tsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
-import {useAppSelector} from "@shared/lib/hooks/hooks.ts";
-import {useRegisterMutation} from "@features/auth/model/api/authApi.ts";
-import styles from './RegisterForm.module.scss'
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppSelector } from "@shared/lib/hooks/hooks.ts";
+import { useRegisterMutation } from "@features/auth/model/api/authApi.ts";
+import styles from "./RegisterForm.module.scss";
 import MyInput from "@shared/ui/input/MyInput.tsx";
 import MyButton from "@shared/ui/button/MyButton.tsx";
+import { useTranslation } from "react-i18next";
+import { renderWithLineBreaks } from "@shared/lib/utils/renderWithLineBreaks";
 
 interface RegisterForm {
   username: string;
@@ -13,83 +15,78 @@ interface RegisterForm {
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState<RegisterForm>({
-    username: '',
-    password: ''
-  })
-  const [register, {isLoading, isSuccess}] = useRegisterMutation()
-  const navigate = useNavigate()
-  const {isAuthenticated} = useAppSelector(state => state.auth)
+    username: "",
+    password: "",
+  });
+  const [register, { isLoading, isSuccess }] = useRegisterMutation();
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (isAuthenticated) {
-      navigate('/profile')
+      navigate("/profile");
     }
   }, [isAuthenticated]);
 
   useEffect(() => {
     if (isSuccess) {
-      navigate('/login')
+      navigate("/login");
     }
   }, [isSuccess]);
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      await register(formData).unwrap()
+      await register(formData).unwrap();
     } catch (err) {
-      console.log('Registration failed', err)
+      console.log("Registration failed", err);
     }
-  }
+  };
 
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
-    setFormData(prev => ({
+    const { name, value } = e.target;
+    setFormData((prev) => ({
       ...prev,
-      [name]: value
-    }))
-  }
+      [name]: value,
+    }));
+  };
   return (
     <div className={styles.container}>
-        <div className={styles.header}>
-          <div className={styles.logo}>Backerly</div>
-          <h3>Register</h3>
-          <p>Create your account. It's free and only takes a minute</p>
-        </div>
-        <form
-          className={styles.form}
-          onSubmit={handleSubmit}
-        >
-          <MyInput
-            name='username'
-            placeholder="Username"
-            value={formData.username}
-            onChange={handleChange}
-          ></MyInput>
-          <MyInput
-            name='password'
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleChange}
-            type='password'
-            showPasswordToggle={true}
-          ></MyInput>
+      <div className={styles.header}>
+        <div className={styles.logo}>Backerly</div>
+        <h3>{renderWithLineBreaks(t("registerForm.title"))}</h3>
+        <p>{renderWithLineBreaks(t("registerForm.desc"))}</p>
+      </div>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <MyInput
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        ></MyInput>
+        <MyInput
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          type="password"
+          showPasswordToggle={true}
+        ></MyInput>
 
-          <MyButton
-            type='submit'
-            disabled={isLoading}
-            size='FULL'
-          >Create Account</MyButton>
-        </form>
+        <MyButton type="submit" disabled={isLoading} size="FULL">
+          {renderWithLineBreaks(t("registerForm.btn"))}
+        </MyButton>
+      </form>
       <div className={styles.navigate}>
-        Already have an account?
-        <Link
-          to='/login'
-          className={styles.link}
-        >Sign up</Link>
+        {renderWithLineBreaks(t("registerForm.nav"))}
+        <Link to="/login" className={styles.link}>
+          {renderWithLineBreaks(t("registerForm.navLink"))}
+        </Link>
       </div>
-      </div>
-
+    </div>
   );
 };
 
