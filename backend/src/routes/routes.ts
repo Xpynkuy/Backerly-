8382a55b8dd4c5edd3f85e07b539f2bc51c14f3d
Andrew <andrey_pgg@mail.ru>
2026-01-02 21:57:25 +0500

    delete auth middleware

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,12 +1,11 @@
 import { Request, Response, Router } from "express";
 import authController from "../controllers/authController";
-import { authenticate } from "../middleware/authMIddleware";
 
 const router = Router();
 
 router.post("/register", authController.registration);
 router.post("/login", authController.login);
 router.post("/refresh", authController.refresh);
-router.post("/logout", authenticate, authController.logout);
+router.post("/logout", authController.logout);
 
 export default router;
