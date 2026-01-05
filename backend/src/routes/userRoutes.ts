1bd3b3fbc2e2daddca514927f3d9223e1f015777
Andrew <andrey_pgg@mail.ru>
2026-01-05 20:14:18 +0500

    add new user routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,27 @@
 import { Router } from "express";
-import { getUserByUsername } from "../controllers/userController";
+import {
+  getUserByUsername,
+  updateAvatarByUsername,
+  updateBannerByUsername,
+} from "../controllers/userController";
+import { authenticate } from "../middleware/authMIddleware";
+import { uploadMemory } from "../config/uploadMemory";
 
 const router = Router();
 
 router.get("/:username", getUserByUsername);
+
+router.patch(
+  "/:username/avatar",
+  authenticate,
+  uploadMemory.single("avatar"),
+  updateAvatarByUsername
+);
+router.patch(
+  "/:username/banner",
+  authenticate,
+  uploadMemory.single("banner"),
+  updateBannerByUsername
+);
 
 export default router;
