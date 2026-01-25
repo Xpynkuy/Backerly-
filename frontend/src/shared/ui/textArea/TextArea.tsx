b4b9c16f30779e7245af748cfdc79c6f07a58f0c
Andrew <andrey_pgg@mail.ru>
2026-01-25 23:36:05 +0500

    update textArea comp

--- frontend/src/shared/ui/textArea/TextArea.tsx
+++ frontend/src/shared/ui/textArea/TextArea.tsx
@@ -1,18 +1,28 @@
+import type { CSSProperties } from "react";
 import styles from "./TextArea.module.scss";
 
 interface TextAreaProps {
   placeholder?: string;
   value: string;
+  width?: string;
+  height?: string;
   onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
 }
 
 const TextArea = (props: TextAreaProps) => {
-  const { placeholder, value, onChange } = props;
+  const { placeholder, value, onChange, width, height } = props;
+
+  const TextAreaStyles: CSSProperties = {
+    width: width || "100px",
+    height: height || "200px",
+  };
+
   return (
     <textarea
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      style={TextAreaStyles}
       className={styles.textArea}
     />
   );
