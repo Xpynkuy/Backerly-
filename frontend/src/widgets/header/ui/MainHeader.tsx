a362ada7467d516c04ebf4b9262e4456de2cc567
Andrew <andrey_pgg@mail.ru>
2026-01-25 23:37:02 +0500

    delete logout logic

--- frontend/src/widgets/header/ui/MainHeader.tsx
+++ frontend/src/widgets/header/ui/MainHeader.tsx
@@ -1,29 +1,16 @@
-import { useNavigate } from "react-router-dom";
-import { useLogoutMutation } from "@features/auth/model/api/authApi";
-import MyButton from "@shared/ui/button/MyButton";
 import styles from "./Header.module.scss";
 import { LangSwitch } from "@features/langSwitch";
+import { DropDown } from "@shared/ui/dropDown/DropDown";
 
 const MainHeader = () => {
-  const [logout] = useLogoutMutation();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    try {
-      await logout().unwrap();
-      navigate("/");
-    } catch (error) {
-      console.log("Logout is failed", error);
-    }
-  };
   return (
     <header className={styles.header}>
-      <a href="/feed" className={styles.logo}>
+      <a href="/" className={styles.logo}>
         Backerly
       </a>
       <div className={styles.nav__btn}>
         <LangSwitch />
-        <MyButton onClick={handleLogout}>Logout</MyButton>
+        <DropDown />
       </div>
     </header>
   );
