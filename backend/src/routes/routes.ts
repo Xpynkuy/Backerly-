b1ba6c5e24337368be5e2e4f53a7c79783ddd345
Andrew <andrey_pgg@mail.ru>
2026-02-09 13:57:46 +0500

    fix auth routes

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import authController from "../controllers/authController";
+import { authenticate } from "../middleware/authMIddleware";
 
 const router = Router();
 
@@ -7,5 +8,6 @@ router.post("/register", authController.registration);
 router.post("/login", authController.login);
 router.post("/refresh", authController.refresh);
 router.post("/logout", authController.logout);
+router.get("/me", authenticate, authController.me)
 
 export default router;
