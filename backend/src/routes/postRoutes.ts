4c3a47d3d89f7addc8373f7b8b5b1e168d1b0aa5
Andrew <andrey_pgg@mail.ru>
2026-02-09 13:56:52 +0500

    add post routes

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -1,28 +1,12 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authMIddleware";
 import { uploadMemory } from "../config/uploadMemory";
-import {
-  createPostByUsername,
-  getPostsByUsername,
-} from "../controllers/postController";
+import { createPostByUsername, getPostsByUsername } from "../controllers/postController";
+import { optionalAuthenticate } from "../middleware/optionalAuthenticate";
 
 const router = Router();
 
-/**
- * ВАЖНО:
- * Этот router будет монтироваться на /api/users
- * => здесь пути начинаются НЕ с /users, а сразу с /:username/...
- */
-
-// GET /api/users/:username/posts
-router.get("/:username/posts", getPostsByUsername);
-
-// POST /api/users/:username/posts
-router.post(
-  "/:username/posts",
-  authenticate,
-  uploadMemory.single("image"),
-  createPostByUsername
-);
+router.get("/:username/posts", optionalAuthenticate, getPostsByUsername);
+router.post("/:username/posts", authenticate, uploadMemory.single("image"), createPostByUsername);
 
 export default router;
