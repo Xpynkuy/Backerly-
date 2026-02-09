2874d38a144f4cbba63f7d2ece7f45af8e4f818a
Andrew <andrey_pgg@mail.ru>
2026-02-09 14:07:02 +0500

    delete console log

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,9 +2,8 @@ import prisma from "./prisma";
 
 export async function connectDB() {
   try {
-    console.log("Попытка подключения к БД");
     await prisma.$connect();
-    console.log("✅ Подключено к БД");
+    console.log("Подключено к БД");
     return true;
   } catch (error) {
     console.log("❌ Ошибка подключения к БД", error);
