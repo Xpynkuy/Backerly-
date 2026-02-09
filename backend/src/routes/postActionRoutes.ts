8ee57081c190feb5d98c80b6410be15324554e1a
Andrew <andrey_pgg@mail.ru>
2026-02-09 13:57:16 +0500

    add post action routes

--- backend/src/routes/postActionRoutes.ts
+++ backend/src/routes/postActionRoutes.ts
@@ -1,23 +1,12 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authMIddleware";
-import { toggleLike, getComments, addComment } from "../controllers/postController";
+import { toggleLike, getComments, addComment, deletePost } from "../controllers/postController";
 
 const router = Router();
 
-/**
- * Этот router будет монтироваться на /api/posts
- * => итоговые пути будут:
- * /api/posts/:id/like
- * /api/posts/:id/comments
- */
-
-// POST /api/posts/:id/like
 router.post("/:id/like", authenticate, toggleLike);
-
-// GET /api/posts/:id/comments
 router.get("/:id/comments", getComments);
-
-// POST /api/posts/:id/comments
 router.post("/:id/comments", authenticate, addComment);
+router.delete("/:id", authenticate, deletePost);
 
 export default router;
