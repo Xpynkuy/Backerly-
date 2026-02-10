f6664b23c8cba62f24b506c9a747601d693fc711
Andrew <andrey_pgg@mail.ru>
2026-02-10 07:06:19 +0500

    create like btn

--- frontend/src/features/postLike/ui/PostLikeButton.tsx
+++ frontend/src/features/postLike/ui/PostLikeButton.tsx
@@ -1,8 +1,7 @@
-import { useToggleLikeMutation, postApi } from "@entities/post/model/api/postApi";
-import { useAppDispatch } from "@shared/lib/hooks/hooks";
+import { useToggleLikeMutation } from "@entities/post/model/api/postApi";
 import MyButton from "@shared/ui/button/MyButton";
 import { Heart } from "lucide-react";
-
+import styles from "./PostLikeButton.module.scss";
 
 interface PostLikeButtonProps {
   username: string;
@@ -12,31 +11,24 @@ interface PostLikeButtonProps {
 }
 
 export const PostLikeButton = (props: PostLikeButtonProps) => {
-  const {username, postId, currentLikesCount, liked} = props
-  const dispatch = useAppDispatch();
-
+  const { username, postId, currentLikesCount, liked } = props;
   const [toggleLike, { isLoading }] = useToggleLikeMutation();
 
-  const onClick = async () => {
-    const res = await toggleLike({ postId }).unwrap();
-
-    dispatch(
-      postApi.util.updateQueryData(
-        "getProfilePosts",
-        { username, take: 5, cursor: null },
-        (draft) => {
-          const p = draft.items.find((x) => x.id === postId);
-          if (!p) return;
-
-          p._count.likes = res.likesCount;
-        }
-      )
-    );
-  };
+  const onClick = () => toggleLike({ postId, username });
 
   return (
-    <MyButton icon={<Heart className={liked ? "likedHeart" : ""}/>} onClick={onClick} disabled={isLoading} color="TRANSPARENT" size="AUTO">
-       {currentLikesCount}
+    <MyButton
+      onClick={onClick}
+      disabled={isLoading}
+      color="TRANSPARENT"
+      size="AUTO"
+    >
+      <div className={styles.btnContainer}>
+        <Heart className={liked ? styles.likedHeart : styles.heart} />
+        <span className={liked ? styles.likedCount : ""}>
+          {currentLikesCount}
+        </span>
+      </div>
     </MyButton>
   );
 };
