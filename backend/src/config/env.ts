459d5302b5170c896e5f82948d9f2b12fb442d61
Andrew <andrey_pgg@mail.ru>
2026-03-01 02:49:53 +0500

    fix access token

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 5001;
 export const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
-export const ACCESS_TOKEN_EXPIRES_IN = '15m';
+export const ACCESS_TOKEN_EXPIRES_IN = '6h';
 export const REFRESH_TOKEN_EXPIRES_IN = '7d';
 
 if (!JWT_ACCESS_SECRET || !JWT_REFRESH_SECRET) {
